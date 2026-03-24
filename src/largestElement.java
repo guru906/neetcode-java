7e0c5d7b9f2b2dacd137860bd6bd55c237be0e2f
guru906 <guruprasanth014@gmail.com>
2026-03-25 05:23:17 +0530

    Day01 - Largest Element added

---
+++ src/largestElement.java
@@ -0,0 +1,19 @@
+//git add .
+//git commit -m "Day01 - Largest Element added"
+//git push
+
+public class largestElement{
+    public static void main(String args[])
+    {
+        int arr[]={1,7,4,9,3,7,3};
+        int largest =arr[0];
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i]>largest)
+            {
+                largest = arr[i];
+            }
+        }
+        System.out.println("LARGEST ELEMENT IS : "+largest);
+    }
+}
