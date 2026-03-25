c5ce0ae9e11b5c155dfdc0c7c6531fb15d3235a3
guru906 <guruprasanth014@gmail.com>
2026-03-25 05:35:52 +0530

    Day01 -SecondLargest Element added

--- src/secondlargest.java
+++ src/secondlargest.java
@@ -1,2 +1,24 @@
 public class secondlargest {
+    public static void main(String args[])
+    {
+        int arr[]={1,7,4,9,3,7,3};
+        int largest =arr[0];
+        int secondlargest =-1;
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i]>largest)
+            {
+                secondlargest =largest;
+                largest = arr[i];
+
+            }
+            else if(arr[i]<largest && arr[i]>secondlargest)
+            {
+                secondlargest =arr[i];
+            }
+        }
+        System.out.println("LARGEST ELEMENT IS : "+largest);
+        System.out.println("SECOND LARGEST ELEMENT IS : "+secondlargest);
+
+    }
 }
